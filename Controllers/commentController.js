6718f607700437c4c54daf82f2032d67bf8528fa
Cleffy <falchard@live.com>
2023-09-30 19:35:49 -0700

    Added Comments Table

--- Controllers/commentController.js
+++ Controllers/commentController.js
@@ -1,7 +1,125 @@
-//get comments from post ID
+const ( Comment ) = require('../Models');
 
-//get comment from comment ID
+/**
+ * @function getPostComments
+ * @param {INT} id - id of Post
+ * @returns Comment data
+ */
+async function getPostComments(id){
+    try {
+        const CommentData = await Comment.findAll(
+            {
+                where: { post_id: id }
+            });
+        if(!CommentData) {
+            return Promise.resolve(
+                {
+                    status: 400,
+                    message: 'Comments do not exist'
+                });
+        }
 
-//get comment from user ID
+        return CommentData;
+    }
+    catch(error) {
+        console.error(error);
+        return Promise.resolve(
+            {
+                status: 500,
+                message: 'Error getting comments'
+            });
+    }
+}
+/**
+ * @function getUserComments
+ * @param {INT} id - id of User
+ * @returns Comment data
+ */
+async function getUserComments(id){
+    try {
+        const CommentData = await Comment.findAll(
+            {
+                where: { user_id: id }
+            });
+        if(!CommentData) {
+            return Promise.resolve(
+                {
+                    status: 400,
+                    message: 'Comments do not exist'
+                });
+        }
 
-//delete comment by ID
+        return CommentData;
+    }
+    catch(error) {
+        console.error(error);
+        return Promise.resolve(
+            {
+                status: 500,
+                message: 'Error getting comments'
+            });
+    }
+}
+
+
+/**
+ * @function getComment
+ * @param {INT} id - id of Comment
+ * @returns Comment data
+ */
+async function getComment(id){
+    try {
+        const CommentData = await Comment.findOne(
+            {
+                where: { id: id }
+            });
+        if(!CommentData) {
+            return Promise.resolve(
+                {
+                    status: 400,
+                    message: 'Comment does not exist'
+                });
+        }
+
+        return CommentData;
+    }
+    catch(error) {
+        console.error(error);
+        return Promise.resolve(
+            {
+                status: 500,
+                message: 'Error getting a comment'
+            });
+    }
+}
+
+/**
+ * @function deleteComment
+ * @param {INT} id - ID of comment
+ * @returns Status
+ */
+async function deleteComment(id){
+    try {
+        await Post.destroy(
+            {
+                where: { id: id }
+            }
+        );
+        return Promise.resolve(
+            {
+                status: 200,
+                message: 'Deleted comment'
+            }
+        );
+    }
+    catch(error) {
+        console.error(error);
+        return Promise.resolve(
+            {
+                status: 500,
+                message: 'Error deleting comment'
+            });
+    }
+}
+
+module.exports = { getComment, getPostComments, getUserComments, deleteComment };
--- Routes/API/commentRoutes.js
+++ Routes/API/commentRoutes.js
@@ -1,7 +1,93 @@
-//get comments from post ID
+const router = require('express').Router();
+const { 
+    getComment, 
+    getPostComments, 
+    getUserComments, 
+    deleteComment 
+} = require('../../Controllers/commentController');
 
-//get comment from comment ID
 
-//get comment from user ID
+/**
+ * Get Comment
+ * request.params
+ *      id - Comment ID
+ */
+router.get('/single/:id', async (request, response) => {
+    try {
+        const commentData = await getComment(request.params.id);
+        if(commentData.status == 400 || commentData.status == 500) {
+            response
+                .status(commentData.status)
+                .json({message: commentData.message});
+            return;
+        }
+        response
+            .status(200)
+            .json(commentData);
+    }
+    catch(error) {
+        response.status(500).json(error);
+    }
+});
 
-//delete comment by ID
+/**
+ * Get Post's Comments
+ * request.params
+ *      id - User ID
+ */
+router.get('/posts/:id', async (request, response) => {
+    try {
+        const commentData = await getPostComments(request.params.id);
+        if(commentData.status == 400 || commentData.status == 500) {
+            response
+                .status(commentData.status)
+                .json({message: commentData.message});
+            return;
+        }
+        response
+            .status(200)
+            .json(commentData);
+    }
+    catch(error) {
+        response.status(500).json(error);
+    }
+});
+
+/**
+ * Get User's Comments
+ * request.params
+ *      id - User ID
+ */
+router.get('/users/:id', async (request, response) => {
+    try {
+        const commentData = await getUserComments(request.params.id);
+        if(commentData.status == 400 || commentData.status == 500) {
+            response
+                .status(commentData.status)
+                .json({message: commentData.message});
+            return;
+        }
+        response
+            .status(200)
+            .json(commentData);
+    }
+    catch(error) {
+        response.status(500).json(error);
+    }
+});
+
+/**
+ * Delete Comment
+ * request.params
+ *      id - Comment ID
+ */
+router.delete('/:id', async (request, response) => {
+    try {
+        return await deleteComment(request.params.id);
+    }
+    catch(error) {
+        response.status(500).json(error);
+    }
+});
+
+module.exports = router;
