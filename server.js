1fd2ae613d7d71ac5a0f53805914f18a6313ec08
Cleffy <falchard@live.com>
2023-10-15 19:07:17 -0700

    Removed engines, removed helpers

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ /*helpers*/ });
 
 app.use(session(
     {
