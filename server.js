718b62709bcf8a111dab2dbb7d7f5aa758c23cd3
Cleffy <falchard@live.com>
2023-10-15 21:37:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,48 @@
-//Import path
 const path = require('path');
-
-//Import express, session, handlebars
 const express = require('express');
 const session = require('express-session');
-const { engine } = require('express-handlebars');
-
-//Import routes, helpers, config
+const exphbs = require('express-handlebars');
 const routes = require('./routes');
-//const helpers = require('./Utils/helpers');
-//require('dotenv').config();
 
-//Get sequelize connectionnpm
+require('dotenv').config();
+
+//Import sequelize connection
 const sequelize = require('./config/connection');
 //Create a Sequelize Store for an express session
-const sequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 //Start express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const engine = exphbs.create({});
+
 const sess = {
     secret: process.env.SESSION_SECRET,
     cookie: {
         maxAge: 30*24*60*60*1000,
-        httpOnly: true,
-        secure: false, 
-        sameSite: 'strict',
     },
     resave: false,
     saveUninitialized: true,
-    store: new sequelizeStore({
+    store: new SequelizeStore({
         db: sequelize
     })
 };
 
 app.use(session(sess));
-app.use(function (request, response, next){
-    response.locals.session = request.session;
-    next();
-});
 
 //Create a handlebars engine and apply it to express
-app.engine('handlebars', engine());
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.set('views', './Views');
+app.set('views', './views');
 
 //Configure express with routes
-app.use(express.json({}));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 //Initialize express
-sequelize.sync().then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening on port: ' + PORT));
 });
